511c42c562bebdebc8c5c2f0f72f255c34a28424
masum691 <masumhu691@gmail.com>
2021-09-04 18:47:28 +0600

    added apikey and input field

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log('connected')
+/* api link: 
+api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+api key:
+26912324e6c8ba022d234b0d4a996765
+*/
+
+const apiKey = `26912324e6c8ba022d234b0d4a996765`
+const searchWeatherButton = () => {
+    const city = document.getElementById('search-input').value;
+    const url = `
+    https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}
+    `
+    document.getElementById('search-input').value = '';
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data.main.temp))
+}

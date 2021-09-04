954aa83539a6c389555ac48729a37d1d1683b4b1
masum691 <masumhu691@gmail.com>
2021-09-04 19:17:52 +0600

    city and temperature

--- js/app.js
+++ js/app.js
@@ -14,5 +14,19 @@ const searchWeatherButton = () => {
     document.getElementById('search-input').value = '';
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data.main.temp))
+    .then(data => displayWeather(data))
+}
+const displayWeather = temp => {
+    console.log(temp)
+    const tempStatus = document.getElementById('temp-status');
+    tempStatus.textContent = '';
+    const div = document.createElement('div');
+    div.classList.add('weather-status');
+    div.innerHTML = `
+        <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="">
+        <h1>${temp.name}</h1>
+        <h3><span>${(temp.main.temp - 273.15).toFixed(2)}</span>&deg;C</h3>
+        <h1 class="lead">Clouds</h1>
+    `
+    tempStatus.appendChild(div);
 }
